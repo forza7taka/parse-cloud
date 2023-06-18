52b76192445a67393924c326e3279522d9388102
forza7taka <forza7.retainer.metropolis@gmail.com>
2023-06-18 09:55:59 +0900

    dele log emailaddress

--- cloud/main.js
+++ cloud/main.js
@@ -8,21 +8,17 @@ Parse.Cloud.define("GoogleSignIn", async (request) => {
         !process.env.GOOGLE_REDIRECT_URI) {
         return
     }
-    try {
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.GOOGLE_CLIENT_ID,
-            process.env.GOOGLE_CLIENT_SECRET,
-            process.env.GOOGLE_REDIRECT_URI
-        )
-        const scopes = ['openid', 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
-        const url = oauth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: scopes
-        })
-        return url
-    } catch (e) {
-        throw e
-    }
+    const oauth2Client = new google.auth.OAuth2(
+        process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_CLIENT_SECRET,
+        process.env.GOOGLE_REDIRECT_URI
+    )
+    const scopes = ['openid', 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
+    const url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes
+    })
+    return url
 });
 
 Parse.Cloud.define("GoogleGetID", async (request) => {
@@ -34,30 +30,23 @@ Parse.Cloud.define("GoogleGetID", async (request) => {
         return
     }
 
-    try {
-        const { google } = require('googleapis')
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.GOOGLE_CLIENT_ID,
-            process.env.GOOGLE_CLIENT_SECRET,
-            process.env.GOOGLE_REDIRECT_URI
-        )
-        const { tokens } = await oauth2Client.getToken(request.params.code)
-        console.log(tokens)
-        oauth2Client.setCredentials({ access_token: tokens.access_token })
+    const { google } = require('googleapis')
+    const oauth2Client = new google.auth.OAuth2(
+        process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_CLIENT_SECRET,
+        process.env.GOOGLE_REDIRECT_URI
+    )
+    const { tokens } = await oauth2Client.getToken(request.params.code)
+    oauth2Client.setCredentials({ access_token: tokens.access_token })
 
-        const api = google.people({ version: 'v1', auth: oauth2Client })
-        const people = await api.people.get({
-            resourceName: 'people/me',
-            personFields: 'emailAddresses'
-        })
-        console.log(people)
-        return {
-            access_token: tokens.access_token,
-            id_token: tokens.id_token,
-            email: people.data.emailAddresses[0].value,
-            id: people.data.emailAddresses[0].metadata.source.id
-        }
-    } catch (e) {
-        throw e
+    const api = google.people({ version: 'v1', auth: oauth2Client })
+    const people = await api.people.get({
+        resourceName: 'people/me',
+        personFields: 'emailAddresses'
+    })
+    return {
+        access_token: tokens.access_token,
+        id_token: tokens.id_token,
+        id: people.data.emailAddresses[0].metadata.source.id
     }
 });
